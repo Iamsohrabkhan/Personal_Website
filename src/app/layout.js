e4df8b785f7499fc9969b8dca72457234e78ff4b
Sohrab Khan <7ohrabkhan@gmail.com>
2025-05-28 16:06:00 +0500

    Add Vercel Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import './globals.css'
-
+import { Analytics } from '@vercel/analytics/next';
 export const metadata = {
   title: {
     default: "Sohrab Khan",
@@ -15,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <body >{children}
+      <Analytics />
+      </body>
     </html>
   )
 }
